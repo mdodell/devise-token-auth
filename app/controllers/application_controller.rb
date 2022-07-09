2dcd69d7c8d34536be03a4c2a886bde91ea905b2
mdodell <dodellmitchell@gmail.com>
2022-07-09 14:55:28 -0400

    removed authenticate user method from application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,4 @@ class ApplicationController < ActionController::API
         include ActionController::Cookies
         include DeviseTokenAuth::Concerns::SetUserByToken
 
-        
-        def authenticate_user!
-                unless current_user
-                  return render json: {
-                    errors: ["You shall not pass!"]
-                  }, status: 401
-                end
-        end
 end
